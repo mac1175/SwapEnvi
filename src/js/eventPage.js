efa9b2dbf5c643a33d650be7f4038643501f9de9
Mitch <mac1175@gmail.com>
2015-04-28 06:41:11 -0400

    enhancements

--- src/js/eventPage.js
+++ src/js/eventPage.js
@@ -13,13 +13,14 @@ if (!chrome.runtime) {
     chrome.runtime.connect = chrome.extension.connect;
 }
 
-function changeTabUrlByDomain(tab, domain) {
+function changeTabUrlByDomain(tab, domain, keepPath) {
     var url = new URL(tab.url);
     url.host = domain.url.host;
     url.protocol = domain.url.protocol;
     //if (domain2.url.port != "")
     //    url.port = domain2.url.port;
-    chrome.tabs.update(tab.id, {url: url.href});
+    var href= keepPath ? url.href : url.origin;
+    chrome.tabs.update(tab.id, {url: href });
 };
 chrome.tabs.onActivated.addListener(function () {
     chrome.storage.sync.get('savedDomains', function (data) {
@@ -37,9 +38,7 @@ chrome.tabs.onActivated.addListener(function () {
 function loadDomains(d) {
     var domains = _.sortBy(d, "label");
     chrome.contextMenus.removeAll(function () {
-        /*var root = chrome.contextMenus.create({
-         title: 'Envi Options', "contexts": ['page']
-         });*/
+
 
         _.forEach(domains, function (domain) {
             var targetUrl = domain.url.origin + '/*';
@@ -52,12 +51,33 @@ function loadDomains(d) {
             _.forEach(otherDomains, function (domain2) {
                 console.log("domain2", domain2);
                 console.log("targetUrl", targetUrl);
+                var root = chrome.contextMenus.create({
+                    title: 'Go to \''+domain2.label + '\' ...',
+                    documentUrlPatterns: [targetUrl],
+                    "contexts": ['all']
+                });
                 chrome.contextMenus.create({
-                    title: 'Change to ' + domain2.label + ' [' + domain2.url.origin + ']',
-                    documentUrlPatterns:[targetUrl],
+                    title: 'With current path',
+                    documentUrlPatterns: [targetUrl],
+                    parentId: root,
                     "contexts": ['all'],
                     onclick: function () {
-                        chrome.tabs.query({url: targetUrl}, function (tabs) {
+                        //asume documentUrlPatterns worked, we don't need targetUrl
+                        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+                            _.forEach(tabs, function (tab) {
+                                changeTabUrlByDomain(tab, domain2);
+                            });
+                        });
+                    }
+                });
+                chrome.contextMenus.create({
+                    title: 'Without current path',
+                    documentUrlPatterns: [targetUrl],
+                    "contexts": ['all'],
+                    parentId: root,
+                    onclick: function () {
+                        //asume documentUrlPatterns worked, we don't need targetUrl
+                        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
                             _.forEach(tabs, function (tab) {
                                 changeTabUrlByDomain(tab, domain2);
                             });
@@ -66,7 +86,7 @@ function loadDomains(d) {
                 });
             });
             chrome.contextMenus.create({
-                title: 'Change ALL other environments to this one. ' + targetUrl,
+                title: 'Change ALL other tabs with the other selected hosts to use this one.',
                 documentUrlPatterns: [targetUrl],
                 "contexts": ['all'],
                 onclick: function () {
@@ -78,16 +98,16 @@ function loadDomains(d) {
                 }
             });
             chrome.contextMenus.create({
-                title: 'Open ALL other environments using their host. ' + targetUrl,
+                title: 'Open ALL other environments using their host. ',
                 documentUrlPatterns: [targetUrl],
                 "contexts": ['all'],
                 onclick: function () {
                     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
                         var tab = tabs[0];
                         if (tab) {
-                            chrome.tabs.duplicate(tab.id, function (dupeTab) {
-                                _.forEach(otherDomains, function (dd) {
-                                    changeTabUrlByDomain(dupeTab, dd);
+                            _.forEach(otherDomains, function (domain2) {
+                                chrome.tabs.duplicate(tab.id, function (dupeTab) {
+                                    changeTabUrlByDomain(dupeTab, domain2);
                                 });
                             });
                         }
